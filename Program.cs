66370aeaa9c65e5f4147cc2c7dd6f3f974ce65df
Aziz-pang <zhiming3399@gmail.com>
2023-07-21 23:52:57 +0800

    feat: 添加 Task html 的案例

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime;
 using System.Text;
 
-string fileName = @"c:\temp\1.txt";
-
-StringBuilder sb = new StringBuilder();
-for(int i= 100 - 1; i >= 0; i--)
+internal class Program
 {
-    sb.AppendLine("hello world. async");
-}
-// File.WriteAllText(fileName, "hello world!");
-await File.WriteAllTextAsync(fileName, sb.ToString());
+    private static async Task Main(string[] args)
+    {
+        await WriteFileAsync(@"C:\temp\1.txt", "hello async");
+        int htmlLength = await DownloadHtmlAsync("https://github.com/Aziz-pang/", @"C:\temp\2.txt");
+        Console.WriteLine("html 长度为: " + htmlLength);
+    }
 
-string str = await File.ReadAllTextAsync(fileName);
-Console.WriteLine(str);
+    private static async Task WriteFileAsync(string path, string contents)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 10 - 1; i >= 0; i--)
+        {
+            sb.AppendLine(contents);
+        }
+        await File.WriteAllTextAsync(path, sb.ToString());
+
+        string str = await File.ReadAllTextAsync(path);
+        Console.WriteLine(str);
+    }
+
+    private static async Task<int> DownloadHtmlAsync(string url, string fileName)
+    {
+        using (HttpClient httpClient = new HttpClient())
+        {
+            string html = await httpClient.GetStringAsync(url);
+            File.WriteAllTextAsync(fileName, html).Wait();
+            Console.WriteLine("is OK!");
+            return html.Length;
+        }
+    }
+}
