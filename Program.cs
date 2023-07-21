07eeadc8a456efff379733a87ae2c56501e01653
Aziz-pang <zhiming3399@gmail.com>
2023-07-21 23:04:52 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+string fileName = @"c:\temp\1.txt";
+
+StringBuilder sb = new StringBuilder();
+for(int i=0; i < 100; i++)
+{
+    sb.AppendLine("hello world. async");
+}
+// File.WriteAllText(fileName, "hello world!");
+await File.WriteAllTextAsync(fileName, sb.ToString());
+
+string str = await File.ReadAllTextAsync(fileName);
+Console.WriteLine(str);
