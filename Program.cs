ff41614ba38c1d7d8d45a9ea0d9a4c7afa7d4ed9
Aziz-pang <zhiming3399@gmail.com>
2023-07-24 00:45:55 +0800

    feat: 新增 task 异步 CancellationToken 的案例

--- Program.cs
+++ Program.cs
@@ -1,38 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Runtime;
 using System.Text;
 
-internal class Program
+namespace ConsoleApp1
 {
-    private static async Task Main(string[] args)
+    class Program
     {
-        await WriteFileAsync(@"C:\temp\1.txt", "hello async");
-        int htmlLength = await DownloadHtmlAsync("https://github.com/Aziz-pang/", @"C:\temp\2.txt");
-        Console.WriteLine("html 长度为: " + htmlLength);
-    }
-
-    private static async Task WriteFileAsync(string path, string contents)
-    {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 10 - 1; i >= 0; i--)
+        private static async Task Main(string[] args)
         {
-            sb.AppendLine(contents);
+            await TaskCancelToken.MainRun();
         }
-        await File.WriteAllTextAsync(path, sb.ToString());
 
-        string str = await File.ReadAllTextAsync(path);
-        Console.WriteLine(str);
-    }
-
-    private static async Task<int> DownloadHtmlAsync(string url, string fileName)
-    {
-        using (HttpClient httpClient = new HttpClient())
+        private static async Task MainRun()
         {
-            string html = await httpClient.GetStringAsync(url);
-            File.WriteAllTextAsync(fileName, html).Wait();
-            Console.WriteLine("is OK!");
-            return html.Length;
+            await WriteFileAsync(@"C:\temp\1.txt", "hello async");
+            int htmlLength = await DownloadHtmlAsync("https://github.com/Aziz-pang/", @"C:\temp\2.txt");
+            Console.WriteLine("html 长度为: " + htmlLength);
+        }
+
+        private static async Task WriteFileAsync(string path, string contents)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 10 - 1; i >= 0; i--)
+            {
+                sb.AppendLine(contents);
+            }
+            await File.WriteAllTextAsync(path, sb.ToString());
+
+            string str = await File.ReadAllTextAsync(path);
+            Console.WriteLine(str);
+        }
+
+        private static async Task<int> DownloadHtmlAsync(string url, string fileName)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string html = await httpClient.GetStringAsync(url);
+                File.WriteAllTextAsync(fileName, html).Wait();
+                Console.WriteLine("is OK!");
+                return html.Length;
+            }
         }
     }
 }
---
+++ TaskCancelToken.cs
@@ -0,0 +1,80 @@
+﻿namespace ConsoleApp1
+{
+    internal class TaskCancelToken
+    {
+        public static async Task MainRun()
+        {
+            Console.WriteLine("start");
+            string url = "https://github.com/aziz-pang/";
+            int overtime = 5000;
+            // await DownloadAsync(url, 10);
+            CancellationTokenSource cts = new CancellationTokenSource();
+            cts.CancelAfter(overtime);
+            CancellationToken token = cts.Token;
+            try
+            {
+                // CancelAction(cts);
+                await DownloadForCancelTokenThrowError(url, overtime, token);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("请求已超时：" + overtime);
+            }
+        }
+
+        static async Task DownloadAsync(string url, int num)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                for (int i = num - 1; i >= 0; i--)
+                {
+                    string html = await client.GetStringAsync(url);
+                    Console.WriteLine(html);
+                }
+            }
+        }
+
+        static async Task DownloadForCancelToken(string url, int num, CancellationToken token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                for (int i = num - 1; i >= 0; i--)
+                {
+                    string html = await client.GetStringAsync(url);
+                    Console.WriteLine(html);
+                    /** 通过 IsCancellationRequestd 退出 */
+                    /* 
+                    if( token.IsCancellationRequested )
+                    {
+                        Console.WriteLine("cancel");
+                        break;
+                    }
+                    */
+                    token.ThrowIfCancellationRequested();
+                }
+            }
+        }
+
+        static async Task DownloadForCancelTokenThrowError(string url, int num, CancellationToken token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                for (int i = num - 1; i >= 0; i--)
+                {
+                    var resp = await client.GetAsync(url, token);
+                    string html = await resp.Content.ReadAsStringAsync();
+                    Console.WriteLine(html[..50]);
+                }
+            }
+        }
+
+        static void CancelAction(CancellationTokenSource cts)
+        {
+            if (Console.ReadLine() == "q")
+            {
+                Console.WriteLine("手动取消");
+                cts.Cancel();
+            }
+        }
+    }
+}
